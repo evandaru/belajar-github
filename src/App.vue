21645f88cb79338a6f5ca03938b54b1b23695cad
evandaru <fznhsym@gmail.com>
2023-02-15 22:08:03 +0700

    pertama bang

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+
+
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <h1 class="text-center font-mono">persatuan ikan</h1>
+    <h1 class="text-center">persatuan ikan</h1>
+  </header>
+
+
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
